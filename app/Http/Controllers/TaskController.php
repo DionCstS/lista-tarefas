cbe81da08d3263870e03c1fb59d2d2c524780a60
DionCstS <dionathacastro1@gmail.com>
2025-01-10 15:40:39 -0300

    adcionando funcionalidade de criação de tarefa inserindo no banco de dados

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -12,4 +12,23 @@ class TaskController extends Controller
         $tasks = Task::all();
         return view('tasks', compact('tasks'));
     }
+
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function store(Request $request)
+    {
+        $titulo = $request->title;
+        $descricao = $request->description;
+
+        $task = new Task();
+        $task->title = $titulo;
+        $task->description = $descricao;
+        $task->save();
+
+        return redirect()->route('task.index');
+    }
+
 }
